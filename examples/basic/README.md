f65a6bff7ea62de59c2b7cbfc1e21f0f2acf12b3
tomas-balaz <tomasbalaz3012@gmail.com>
2022-07-12 14:29:53 +0200

    feat: update addon to new layout

--- examples/basic/README.md
+++ examples/basic/README.md
@@ -11,10 +11,13 @@ No requirements.
 
 | Name | Source | Version |
 |------|--------|---------|
-| <a name="module_eks_cluster"></a> [eks\_cluster](#module\_eks\_cluster) | cloudposse/eks-cluster/aws | n/a |
-| <a name="module_eks_workers"></a> [eks\_workers](#module\_eks\_workers) | cloudposse/eks-workers/aws | n/a |
-| <a name="module_ingress_nginx"></a> [ingress\_nginx](#module\_ingress\_nginx) | ../../ | n/a |
-| <a name="module_vpc"></a> [vpc](#module\_vpc) | terraform-aws-modules/vpc/aws | n/a |
+| <a name="module_eks_cluster"></a> [eks\_cluster](#module\_eks\_cluster) | cloudposse/eks-cluster/aws | 0.45.0 |
+| <a name="module_eks_node_group"></a> [eks\_node\_group](#module\_eks\_node\_group) | cloudposse/eks-node-group/aws | 0.28.0 |
+| <a name="module_ingress_nginx_argo_helm"></a> [ingress\_nginx\_argo\_helm](#module\_ingress\_nginx\_argo\_helm) | ../../ | n/a |
+| <a name="module_ingress_nginx_argo_kubernetes"></a> [ingress\_nginx\_argo\_kubernetes](#module\_ingress\_nginx\_argo\_kubernetes) | ../../ | n/a |
+| <a name="module_ingress_nginx_disabled"></a> [ingress\_nginx\_disabled](#module\_ingress\_nginx\_disabled) | ../../ | n/a |
+| <a name="module_ingress_nginx_helm"></a> [ingress\_nginx\_helm](#module\_ingress\_nginx\_helm) | ../../ | n/a |
+| <a name="module_vpc"></a> [vpc](#module\_vpc) | terraform-aws-modules/vpc/aws | 3.13.0 |
 
 ## Resources
 
--- variables.tf
+++ variables.tf
@@ -6,8 +6,6 @@ variable "enabled" {
   description = "Variable indicating whether deployment is enabled"
 }
 
-# Helm
-
 variable "helm_create_namespace" {
   type        = bool
   default     = true
@@ -22,7 +20,7 @@ variable "helm_chart_name" {
 
 variable "helm_chart_version" {
   type        = string
-  default     = "3.35.0"
+  default     = "4.1.4"
   description = "Version of the Helm chart"
 }
 
@@ -40,7 +38,7 @@ variable "helm_repo_url" {
 
 # K8s
 
-variable "k8s_namespace" {
+variable "namespace" {
   type        = string
   default     = "ingress-controller"
   description = "The K8s namespace in which the ingress-nginx has been created"
@@ -58,32 +56,25 @@ variable "values" {
   description = "Additional yaml encoded values which will be passed to the Helm chart."
 }
 
-# ArgoCD variables
 variable "argo_namespace" {
   type        = string
   default     = "argo"
   description = "Namespace to deploy ArgoCD application CRD to"
 }
 
-
-variable "argo_application_enabled" {
+variable "argo_enabled" {
   type        = bool
   default     = false
   description = "If set to true, the module will be deployed as ArgoCD application, otherwise it will be deployed as a Helm release"
 }
 
-variable "argo_application_use_helm" {
+variable "argo_helm_enabled" {
   type        = bool
   default     = false
   description = "If set to true, the ArgoCD Application manifest will be deployed using Kubernetes provider as a Helm release. Otherwise it'll be deployed as a Kubernetes manifest. See Readme for more info"
 }
 
-variable "argo_application_values" {
-  default     = ""
-  description = "Value overrides to use when deploying argo application object with helm"
-}
-
-variable "argo_destionation_server" {
+variable "argo_destination_server" {
   type        = string
   default     = "https://kubernetes.default.svc"
   description = "Destination server for ArgoCD Application"
@@ -106,4 +97,220 @@ variable "argo_info" {
 variable "argo_sync_policy" {
   description = "ArgoCD syncPolicy manifest parameter"
   default     = {}
+}
+
+variable "argo_metadata" {
+  default = {
+    "finalizers" : [
+      "resources-finalizer.argocd.argoproj.io"
+    ]
+  }
+  description = "ArgoCD Application metadata configuration. Override or create additional metadata parameters"
+}
+
+variable "argo_apiversion" {
+  default     = "argoproj.io/v1alpha1"
+  description = "ArgoCD Appliction apiVersion"
+}
+
+variable "argo_spec" {
+  default     = {}
+  description = "ArgoCD Application spec configuration. Override or create additional spec parameters"
+}
+
+variable "argo_helm_values" {
+  type        = string
+  default     = ""
+  description = "Value overrides to use when deploying argo application object with helm"
+}
+
+variable "argo_kubernetes_manifest_computed_fields" {
+  type        = list(string)
+  default     = ["metadata.labels", "metadata.annotations"]
+  description = "List of paths of fields to be handled as \"computed\". The user-configured value for the field will be overridden by any different value returned by the API after apply."
+}
+
+variable "argo_kubernetes_manifest_field_manager_name" {
+  default     = "Terraform"
+  description = "The name of the field manager to use when applying the kubernetes manifest resource. Defaults to Terraform"
+}
+
+variable "argo_kubernetes_manifest_field_manager_force_conflicts" {
+  type        = bool
+  default     = false
+  description = "Forcibly override any field manager conflicts when applying the kubernetes manifest resource"
+}
+
+variable "argo_kubernetes_manifest_wait_fields" {
+  type        = map(string)
+  default     = {}
+  description = "A map of fields and a corresponding regular expression with a pattern to wait for. The provider will wait until the field matches the regular expression. Use * for any value."
+}
+
+variable "helm_repo_key_file" {
+  type        = string
+  default     = ""
+  description = "Helm repositories cert key file"
+}
+
+variable "helm_repo_cert_file" {
+  type        = string
+  default     = ""
+  description = "Helm repositories cert file"
+}
+
+variable "helm_repo_ca_file" {
+  type        = string
+  default     = ""
+  description = "Helm repositories cert file"
+}
+
+variable "helm_repo_username" {
+  type        = string
+  default     = ""
+  description = "Username for HTTP basic authentication against the helm repository"
+}
+
+variable "helm_repo_password" {
+  type        = string
+  default     = ""
+  description = "Password for HTTP basic authentication against the helm repository"
+}
+
+variable "helm_devel" {
+  type        = bool
+  default     = false
+  description = "Use helm chart development versions, too. Equivalent to version '>0.0.0-0'. If version is set, this is ignored"
+}
+
+variable "helm_package_verify" {
+  type        = bool
+  default     = false
+  description = "Verify the package before installing it. Helm uses a provenance file to verify the integrity of the chart; this must be hosted alongside the chart"
+}
+
+variable "helm_keyring" {
+  type        = string
+  default     = "~/.gnupg/pubring.gpg"
+  description = "Location of public keys used for verification. Used only if helm_package_verify is true"
+}
+
+variable "helm_timeout" {
+  type        = number
+  default     = 300
+  description = "Time in seconds to wait for any individual kubernetes operation (like Jobs for hooks)"
+}
+
+variable "helm_disable_webhooks" {
+  type        = bool
+  default     = false
+  description = "Prevent helm chart hooks from running"
+}
+
+variable "helm_reset_values" {
+  type        = bool
+  default     = false
+  description = "When upgrading, reset the values to the ones built into the helm chart"
+}
+
+variable "helm_reuse_values" {
+  type        = bool
+  default     = false
+  description = "When upgrading, reuse the last helm release's values and merge in any overrides. If 'helm_reset_values' is specified, this is ignored"
+}
+
+variable "helm_force_update" {
+  type        = bool
+  default     = false
+  description = "Force helm resource update through delete/recreate if needed"
+}
+
+variable "helm_recreate_pods" {
+  type        = bool
+  default     = false
+  description = "Perform pods restart during helm upgrade/rollback"
+}
+
+variable "helm_cleanup_on_fail" {
+  type        = bool
+  default     = false
+  description = "Allow deletion of new resources created in this helm upgrade when upgrade fails"
+}
+
+variable "helm_release_max_history" {
+  type        = number
+  default     = 0
+  description = "Maximum number of release versions stored per release"
+}
+
+variable "helm_atomic" {
+  type        = bool
+  default     = false
+  description = "If set, installation process purges chart on fail. The wait flag will be set automatically if atomic is used"
+}
+
+variable "helm_wait" {
+  type        = bool
+  default     = false
+  description = "Will wait until all helm release resources are in a ready state before marking the release as successful. It will wait for as long as timeout"
+}
+
+variable "helm_wait_for_jobs" {
+  type        = bool
+  default     = false
+  description = "If wait is enabled, will wait until all helm Jobs have been completed before marking the release as successful. It will wait for as long as timeout"
+}
+
+variable "helm_skip_crds" {
+  type        = bool
+  default     = false
+  description = "If set, no CRDs will be installed before helm release"
+}
+
+variable "helm_render_subchart_notes" {
+  type        = bool
+  default     = true
+  description = "If set, render helm subchart notes along with the parent"
+}
+
+variable "helm_disable_openapi_validation" {
+  type        = bool
+  default     = false
+  description = "If set, the installation process will not validate rendered helm templates against the Kubernetes OpenAPI Schema"
+}
+
+variable "helm_dependency_update" {
+  type        = bool
+  default     = false
+  description = "Runs helm dependency update before installing the chart"
+}
+
+variable "helm_replace" {
+  type        = bool
+  default     = false
+  description = "Re-use the given name of helm release, only if that name is a deleted release which remains in the history. This is unsafe in production"
+}
+
+variable "helm_description" {
+  type        = string
+  default     = ""
+  description = "Set helm release description attribute (visible in the history)"
+}
+
+variable "helm_lint" {
+  type        = bool
+  default     = false
+  description = "Run the helm chart linter during the plan"
+}
+
+variable "helm_set_sensitive" {
+  type        = map(any)
+  default     = {}
+  description = "Value block with custom sensitive values to be merged with the values yaml that won't be exposed in the plan's diff"
+}
+
+variable "helm_postrender" {
+  type        = map(any)
+  default     = {}
+  description = "Value block with a path to a binary file to run after helm renders the manifest which can alter the manifest contents"
 }
