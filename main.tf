f9263ae1beb50beaf203684632dfa22e4a9992fd
Matej Hasul <matej.hasul@lablabs.io>
2025-05-13 14:25:31 +0200

    feat(addon): use universal addon

---
+++ main.tf
@@ -0,0 +1,23 @@
+/**
+ * # AWS EKS Ingress Nginx Terraform module
+ *
+ * A Terraform module to deploy the ingress nginx on Amazon EKS cluster.
+ *
+ * [![Terraform validate](https://github.com/lablabs/terraform-aws-eks-ingress-nginx/actions/workflows/validate.yaml/badge.svg)](https://github.com/lablabs/terraform-aws-eks-ingress-nginx/actions/workflows/validate.yaml)
+ * [![pre-commit](https://github.com/lablabs/terraform-aws-eks-ingress-nginx/actions/workflows/pre-commit.yaml/badge.svg)](https://github.com/lablabs/terraform-aws-eks-ingress-nginx/actions/workflows/pre-commit.yaml)
+ */
+# FIXME config: update addon docs above
+locals {
+  # FIXME config: add addon configuration here
+  addon = {
+    name = "ingress-nginx"
+
+    helm_chart_name    = "ingress-nginx"
+    helm_chart_version = "4.1.4"
+    helm_repo_url      = "https://kubernetes.github.io/ingress-nginx"
+  }
+
+  addon_values = yamlencode({
+    # FIXME config: add default values here
+  })
+}
